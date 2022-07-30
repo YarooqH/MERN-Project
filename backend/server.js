dce669aafc15495fa02a9219582de8924fc8f44f
Yarooq Anwar <yarooq1@gmail.com>
2022-07-30 18:17:06 +0500

    backedn setup

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,29 @@
 require('dotenv').config()
+
 const express = require('express');
+const router = require('./routes/handler');
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.json({
-        text: 'Henlo World'
-    })
+app.use(express.json());
+
+app.use((req, res, next) => {
+    // res.header('Access-Control-Allow-Origin', '*');
+    // res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    console.log(req.path, req.method)
+    next();
 })
 
+// app.get('/', (req, res) => {
+//     res.json({
+//         text: 'Henlo World'
+//     })
+// })
+
+app.use('/api', router);
+
 app.listen(process.env.PORT, () => {
-    console.log('Server is running on port' + process.env.PORT);
+    console.log('Server is running on port ' + process.env.PORT);
 })
 
 // process.env
