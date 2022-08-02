ecaea879e41ba247069f729fdbfb804f0f894ee5
Yarooq Anwar <yarooq1@gmail.com>
2022-08-02 22:29:16 +0500

    check

--- frontend/chat-app/src/main.jsx
+++ frontend/chat-app/src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './styles/index.css'
 
-//sad
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
