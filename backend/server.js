62eda884cbd28dbb585bb9d87f0a8e7530c36550
Yarooq Anwar <yarooq1@gmail.com>
2022-08-02 22:28:23 +0500

    check

--- backend/server.js
+++ backend/server.js
@@ -3,27 +3,53 @@ require('dotenv').config()
 const express = require('express');
 const router = require('./routes/handler');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const msg = require('./models/messages');
 
 const io = require('socket.io')(1800);
 
+io.on('connection', (socket) => {
+    socket.emit('message', 'Welcome to the chat');
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+    console.log('user disconnected');
+    });
+});
+
 const app = express();
 
+var corsOptions = {
+    origin: 'http://localhost:5173/',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11) choke on 204
+}
 
-app.use(express.json());
+app.use(()=> {
+    express.json();
+    cors();
+    // console.log("sagdgas")
+    // next();
+});
+// app.use(cors());
 
 app.use((req, res, next) => {
-    // res.header('Access-Control-Allow-Origin', '*');
-    // res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    console.log(req.path, req.method)
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+
+    // Request methods you wish to allow
+    // res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    
+    console.log(req.path, req.method);
+
     next();
 })
 
-// app.get('/', (req, res) => {
+// app.get('/', cors(corsOptions), (req, res) => {
 //     res.json({
 //         text: 'Henlo World'
 //     })
 // })
+
 
 app.use('/api', router);
 
@@ -31,15 +57,7 @@ mongoose.connect(process.env.MONGO_URI)
     .then(() => {
         app.listen(process.env.PORT, () => {
             console.log('Connected to MongoDB');
-            console.log('Server is running on port ' + process.env.PORT);
-            io.on('connection', (socket) => {
-                socket.emit('message', 'Welcome to the chat');
-                console.log('a user connected');
-                socket.on('disconnect', () => {
-                console.log('user disconnected');
-                });
-            });
-            
+            console.log('Server is running on port ' + process.env.PORT);          
         })
     })
     .catch(err => console.log(err));
---
+++ frontend/chat-app/src/App.jsx
@@ -0,0 +1,61 @@
+import socketClient, {io} from "socket.io-client";
+import React, { useEffect, useState } from "react";
+import './styles/App.css'
+
+// var socket = io();
+const socket = socketClient("http://localhost:1700");
+
+function App() {
+  const [isConnected, setIsConnected] = useState(socket.connected);
+  const [lastPong, setLastPong] = useState(null);
+
+
+  useEffect(() => {
+    socket.on('connect', () => {
+      setIsConnected(true);
+    });
+
+    socket.on('disconnect', () => {
+      setIsConnected(false);
+    });
+
+    socket.on('pong', () => {
+      setLastPong(new Date().toISOString());
+    });
+
+    return () => {
+      socket.off('connect');
+      socket.off('disconnect');
+      socket.off('pong');
+    };
+  }, []);
+
+  const sendPing = () => {
+    socket.emit('ping');
+  }
+
+  return (
+    <div>
+      <p>Connected: { '' + isConnected }</p>
+      <p>Last pong: { lastPong || '-' }</p>
+      <button onClick={ sendPing }>Send ping</button>
+    </div>
+  );
+}
+
+// export default App;
+
+
+  // return (
+  //   <div className="App">
+  //     <header className="App-header">
+  //       <form>
+  //         <input type="text" placeholder="Type a message..." />
+  //         <button type="submit">Send</button>          
+  //       </form>
+  //     </header> 
+  //   </div>
+  // )
+// }
+
+export default App
